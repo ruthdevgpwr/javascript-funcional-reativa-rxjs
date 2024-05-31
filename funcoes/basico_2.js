1615fc5071c327836f74e5891afc75263f873e36
Ruth Dantas <ruthdevgp@gmail.com>
2024-05-31 08:33:09 -0300

    add function returning another function in basico_2.js

--- funcoes/basico_2.js
+++ funcoes/basico_2.js
@@ -6,8 +6,28 @@ const boaTarde = function() {
   console.log('Boa tarde!')
 }
 
+// 1) Passar funcao como parametro para outra funcao
+
 function executarQualquerCoisa(funcao){
-  funcao()
+  if(typeof funcao === 'function') {
+    funcao()
+  }
 }
 
 executarQualquerCoisa(boaTarde)
+
+
+// 2) Retornar uma funcao a partir de uma outra func
+
+function potencia(base) {
+  return function(exp) {
+    return Math.pow(base, exp)
+  }
+}
+
+const potenciaDe2 = potencia(2)
+console.log(potenciaDe2(8))
+
+// ou posso chamar diretamente sem armazenar potencia numa constante
+
+console.log(potencia(2)(8))
