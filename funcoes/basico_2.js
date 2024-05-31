c7fb15fc4918b67caf3d35cd6bdca7fa8d3fab5d
Ruth Dantas <ruthdevgp@gmail.com>
2024-05-30 22:00:45 -0300

    implemented code in 'basico_2.js' that demonstrates passing functions as parameters and returning functions from other functions

---
+++ funcoes/basico_2.js
@@ -0,0 +1,13 @@
+function bomDia() {
+  console.log('Bom dia!')
+}
+
+const boaTarde = function() {
+  console.log('Boa tarde!')
+}
+
+function executarQualquerCoisa(funcao){
+  funcao()
+}
+
+executarQualquerCoisa(boaTarde)
