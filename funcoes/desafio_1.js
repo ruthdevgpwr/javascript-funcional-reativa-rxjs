2040e67ea9ccb05d06f7c10261de283ad9fe68a9
Ruth Dantas <ruthdevgp@gmail.com>
2024-06-01 12:08:21 -0300

    add third number to calculation

--- funcoes/desafio_1.js
+++ funcoes/desafio_1.js
@@ -4,16 +4,20 @@
 // fn -> 3 - 7
 // fn calcular (num)(num)(fn)
 
- const soma = (num1, num2) => num1 + num2
- const subtracao = (num1, num2) => num1 - num2
- const multiplicacao = (num1, num2) => num1 * num2
+ const soma = (num1, num2,num3) => num1 + num2 + num3
+ const subtracao = (num1, num2,num3) => num1 - num2 - num3
+ const multiplicacao = (num1, num2,num3) => num1 * num2 * num3
 
 const calcular = function(num1) {
   return function(num2) {
-    return function(operacao) {
-      return operacao(num1, num2)
+    return function (num3) {
+      return function(operacao) {
+        return operacao(num1, num2, num3)
+      }
     }
   }
 }
 
-console.log(calcular(2)(5)(multiplicacao))
+console.log(calcular(100)(10)(10)(multiplicacao))
+console.log(calcular(100)(10)(10)(subtracao))
+console.log(calcular(100)(10)(10)(soma))
