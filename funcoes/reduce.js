669ff28bde07393e2af19516a58d5a9ee6ac97ac
Ruth Dantas <ruthdevgp@gmail.com>
2024-06-21 14:58:53 -0300

    Implemented custom Reduce function in JavaScript

--- funcoes/reduce.js
+++ funcoes/reduce.js
@@ -18,3 +18,22 @@ const totalGeral = carrinho
 
   console.log(totalGeral)
 
+  Array.prototype.meuReduce = function(fn, inicial) {
+    let acc = inicial
+    for(let i = 0; i < this.length; i++) {
+      if(!acc && i === 0) {
+        acc = this[i]
+        continue
+      }
+
+      acc = fn(acc, this[i], i, this)
+    }
+
+    return acc
+  }
+
+ const totalGeral2 = carrinho
+  .map(getTotal)
+  .meuReduce(somar, 0)
+
+  console.log(totalGeral2)
